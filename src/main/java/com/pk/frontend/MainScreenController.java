fc0d00adc15afb6100ca0b7d701eb7cc31627272
Jan Chmielewski <jjjchmielewski@gmail.com>
2022-06-03 22:25:32 +0200

    Add basic screen with input and empty field validation

--- src/main/java/com/pk/frontend/MainScreenController.java
+++ src/main/java/com/pk/frontend/MainScreenController.java
@@ -1,4 +1,34 @@
 package com.pk.frontend;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
 public class MainScreenController {
+
+  @FXML
+  private Button checkButton;
+  @FXML
+  private TextField infoInput;
+  @FXML
+  private Label errorLabel;
+
+  @FXML
+  public void initialize(){
+    errorLabel.setVisible(false);
+
+  }
+
+  public void calculate() {
+    if (!infoInput.getText().isEmpty()) {
+      errorLabel.setVisible(false);
+
+    }
+    else {
+      errorLabel.setVisible(true);
+    }
+  }
 }
