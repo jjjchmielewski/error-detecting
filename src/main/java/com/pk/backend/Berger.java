bfa1bee1ab557f83c4007a3afe2b1546771f5429
Jan Chmielewski <jjjchmielewski@gmail.com>
2022-06-04 17:27:36 +0200

    Add slf4j logger, add first check for berger code

--- src/main/java/com/pk/backend/Berger.java
+++ src/main/java/com/pk/backend/Berger.java
@@ -1,8 +1,32 @@
 package com.pk.backend;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Berger {
 
-  public void check(String binaryString, int infoBits) {
-    int controlBits = (int)((Math.log(infoBits) / Math.log(2)) + 1);
+
+  public static void check(String binaryString) {
+    int controlBitsLength = (int) Math.ceil((Math.log(binaryString.length() - 1.0) / Math.log(2)));
+    int infoBitsLength = binaryString.length() - controlBitsLength;
+    //int infoBitsDecimal = Integer.parseInt(binaryString.substring(0, infoBitsLength), 2);
+    int controlBitsDecimal = Integer.parseInt(binaryString.substring(infoBitsLength), 2);
+    log.info("control bits length " + controlBitsLength);
+    log.info("info bits length " + infoBitsLength);
+    log.info("control bits decimal " + controlBitsDecimal);
+    //log.info("info bits decimal " + infoBitsDecimal);
+
+    int numberOfOnesInInfoBits = 0;
+
+    for(char x : binaryString.substring(0, infoBitsLength).toCharArray()) {
+      if(x == '1') {
+        numberOfOnesInInfoBits++;
+      }
+    }
+
+    if(numberOfOnesInInfoBits != controlBitsDecimal) {
+      log.error("Number of ones is not valid with control bits");
+    }
+
   }
 }
--- src/main/java/com/pk/frontend/MainScreenController.java
+++ src/main/java/com/pk/frontend/MainScreenController.java
@@ -1,5 +1,6 @@
 package com.pk.frontend;
 
+import com.pk.backend.Berger;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -38,7 +39,19 @@ public class MainScreenController {
   public void calculate() {
     if (!infoInput.getText().isEmpty()) {
       errorLabel.setVisible(false);
+      switch (mode) {
+        case CRC -> {
+          break;
+        }
+        case Berger -> {
+          Berger.check(infoInput.getText());
+          break;
+        }
+        case Hamming -> {
+          break;
+        }
 
+      }
     }
     else {
       errorLabel.setVisible(true);
